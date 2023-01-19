d4f18ef61681f28abebeb0d6c390a394b4dd0d67
Victor Ren <victorren2002@gmail.com>
2023-01-18 17:23:36 -0800

    added all files needed

--- app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -1,2 +1,17 @@
-package edu.ucsd.cse110.lab2;public class UtilitiesTest {
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class UtilitiesTest {
+    @Test
+    public void test_trimDisplayStr() {
+        assertEquals("1.234", Utilities.trimDisplayStr("1.2340"));
+        assertEquals("13", Utilities.trimDisplayStr("13.0"));
+
+    }
 }
